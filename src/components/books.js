d00d7582aff8e0d5ade40f3f6def7b8243ec1cac
ilyass atlassi <atlassiilyass@gmail.com>
2023-04-07 19:07:26 +0000

    Add script for api

--- src/components/books.js
+++ src/components/books.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './book';
 import Form from './formBook';
+import { getBooks } from '../redux/books/booksSlice';
 import './styles/Books.css';
 
 export default function Books() {
   const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   return (
     <div className="books">
       <ul className="books-list">
         {books.map((item) => (
           <li key={item.item_id}>
-            <Book title={item.title} author={item.author} id={item.item_id} />
+            <Book
+              title={item.title}
+              author={item.author}
+              category={item.category}
+              id={item.item_id}
+            />
           </li>
         ))}
       </ul>
